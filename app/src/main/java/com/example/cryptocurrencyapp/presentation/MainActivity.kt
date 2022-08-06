3fbbfe4e66b76dd0dd7590b8ba1a308ccc2e5a12
MoeinLatifi <moeinlatifi7117@gmail.com>
2022-08-07 01:27:12 +0430

    added all of the composable funcs to the main activity

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
@@ -10,34 +10,43 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrencyapp.presentation.detail.components.CoinDetailScreen
+import com.example.cryptocurrencyapp.presentation.list.components.CoinsListScreen
 import com.example.cryptocurrencyapp.presentation.theme.CryptoCurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoCurrencyAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinsListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinsListScreen.route
+                        ) {
+                            CoinsListScreen(navController = navController)
+                        }
+
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoCurrencyAppTheme {
-        Greeting("Android")
     }
 }
